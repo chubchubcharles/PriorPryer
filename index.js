620d67fd395fbac8339445dcb4eb8b4ce610f296
MasterOfTheBus <sidney.ng@mail.mcgill.ca>
2014-09-20 00:50:16 -0700

    added client folder for client side logic and ppryer.js for server side logic

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ var path = require('path');
 
 // defines port the server listens on
 http.listen(3000, function(){
-          console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
 // route handler called when website home hit
 app.get('/', function(req, res){
-      // note: sending files will require an absolute path
-      res.sendFile(path.join(path.resolve('.'), 'index.html'));
+    // note: sending files will require an absolute path
+    res.sendFile(path.join(path.resolve('.'), 'index.html'));
 });
 
+// set up socket io to listen on the same port
 var io = require('socket.io').listen(http);
+var serverLogic = require('ppryer.js');
 
 io.on('connection', function(socket){
-      console.log('a user connected');
+    console.log('a user connected');
+      
 });
