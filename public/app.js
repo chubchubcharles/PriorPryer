e12c7e01205f06890c3e8b5915c35601ece4b0ee
Sidney Ng <sidney.ng@mail.mcgill.ca>
2015-01-09 10:07:34 -0500

    added login with facebook; the login window doesn't seem to allow hitting the cancel button, not sure if it's because of my recursion

--- public/app.js
+++ public/app.js
@@ -155,11 +155,40 @@ jQuery(function($){
 
     // hostId : '',
 
+
     onStartClick: function () {
         console.log("CLICKED \"START\": Facebook LOGIN should be prompted here");
         //TODO: Facebook Login Prompt
 
+    var statusChangeCallback = function(response) {
+      console.log('statusChangeCallback');
+      console.log(response);
+      // The response object is returned with a status field that lets the
+      // app know the current login status of the person.
+      // Full docs on the response object can be found in the documentation
+      // for FB.getLoginStatus().
+      if (response.status === 'connected') {
+        // Logged into your app and Facebook.
         IO.socket.emit('startGameAndLogin');
+      } else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+        document.getElementById('status').innerHTML = 'Please log ' +
+          'into this app.';
+      } else {
+        // The person is not logged into Facebook, so we're not sure if
+        // they are logged into this app or not.
+	console.log('not logged in');
+        FB.login(function(response) {
+          statusChangeCallback(response);
+        }, {scope: 'public_profile,email'});
+      }
+    };
+
+        FB.getLoginStatus(function(response) {
+	    console.log('got the status');
+            statusChangeCallback(response);
+        });
+        //IO.socket.emit('startGameAndLogin');
     },
 
     onCreateClick: function () {
